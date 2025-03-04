976bb5bc854d9a9fc999d7f45c0aa7a442b60335
Professor_V2 <78028717+ProfessorV2@users.noreply.github.com>
2025-03-04 11:31:39 +0000

    pp

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '$ZAP',
+  description: 'Zero Action Profit',
+  generator: '$ZAP',
 }
 
 export default function RootLayout({
