37ac3914fee2b12f3895ec735e516d08bc938753
Professor_V2 <78028717+ProfessorV2@users.noreply.github.com>
2025-03-04 11:39:35 +0000

    lkkl

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: '$ZAP',
   description: 'Zero Action Profit',
   generator: '$ZAP',
+  icons: {
+    icon: '/favicon.ico', // Path to your favicon in the /public directory
+  },
 }
 
 export default function RootLayout({
